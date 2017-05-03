1fae37fb0642f7b16ad6ebe04a449953e042b9a1
duongthihonglam <mariahonglam@gmail.com>
2017-05-03 22:45:35 +0700

    Add files via upload

--- ExampleInstrumentedTest.java
+++ ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.example.lamduong.activityislistener;
+package com.example.lamduong.example1;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.lamduong.activityislistener", appContext.getPackageName());
+        assertEquals("com.example.lamduong.example1", appContext.getPackageName());
     }
 }
